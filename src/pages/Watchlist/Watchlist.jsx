44b70350cb302ae3defab0c55a31f3715e22625a
shaikmujafar <shaikmujafar0786@gmail.com>
2023-08-09 21:43:35 +0530

    Localstore

--- src/pages/Watchlist/Watchlist.jsx
+++ src/pages/Watchlist/Watchlist.jsx
@@ -1,16 +1,39 @@
 import React, { useState } from "react";
-import stocksData from '../../common/stocksData.json'
+import stocksData from "../../common/stocksData.json";
 import "./watchlist.css";
 import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
-
 
 import Header from "../../components/Header/Header";
 
-function Watchlist() {
-  const [stocks, setStocks] = useState(stocksData);
+
 
  
+function Watchlist() {
+  const [stocks, setStocks] = useState(stocksData);
+  const navigate = useNavigate(); // Hook for navigation
+
+  const handleSellClick = (stock) => {
+    // Get existing stored stocks from local storage
+    const storedStocks = JSON.parse(localStorage.getItem("SellStocks")) || [];
+
+    // Check if the stock is already in the stored stocks
+    const stockExists = storedStocks.some((storedStock) => storedStock.name === stock.name);
+
+    // If the stock does not exist, add it
+    if (!stockExists) {
+      // Create a new stock object with a quantity of 1
+      const sellStock = { ...stock, quantity: 1 };
+
+      // Add the new stock object to the array
+      storedStocks.push(sellStock);
+
+      // Store the updated array in local storage
+      localStorage.setItem("SellStocks", JSON.stringify(storedStocks));
+    }
+
+    // Navigate to the sell page
+    navigate("/sell");
+  };
 
   return (
     <div>
@@ -23,15 +46,17 @@ function Watchlist() {
           <div className="stock-card-details">
             <h3>{stock.name}</h3>
 
-            <div>Price : {stock.price}</div>
+            <div className="stock-price">Price :  ${stock.price.toFixed(2)}</div>
             <div>Quantity : {stock.quantity}</div>
           </div>
 
           <div className="stock-card-options">
-            <button className="buybtn" >
+            {/* Pass the stock data to the handleSellClick function */}
+            <button className="buybtn" onClick={() => handleSellClick(stock)}>
               Buy
             </button>
-            <button className="sellbtn">
+            {/* Pass the stock data to the handleSellClick function */}
+            <button className="sellbtn" onClick={() => handleSellClick(stock)}>
               Sell
             </button>
           </div>
